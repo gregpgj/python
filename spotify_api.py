43bb3dc4463dc98d348fccb871a3e258e62753a2
gregpgj <gsfr.gui@gmail.com>
2022-12-22 11:02:04 -0300

    modified:   spotify_api.py

--- spotify_api.py
+++ spotify_api.py
@@ -1,8 +1,8 @@
 import spotipy.util as util
 
-username = "12159259511"
-client_id = "8410d0c855f4425186303b8101144993"
-client_secret = "66ec63f759314e84aeea36099bf0e716"
+username = '12159259511'
+client_id = '8410d0c855f4425186303b8101144993'
+client_secret = '66ec63f759314e84aeea36099bf0e716'
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
 
